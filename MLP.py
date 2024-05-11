9a1a8032c5bf44f3658d1c77556971983f6b3b23
shy <shynare@gmail.com>
2024-05-12 03:31:50 +0900

    경로추가

--- MLP.py
+++ MLP.py
@@ -4,9 +4,13 @@ from tensorflow.keras.models import Sequential
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import requests
 
-csv_file_path = ''
-df = pd.read_csv(csv_file_path)
+url = "https://github.com/jinsobak/CMM_DeepLearning_Module/blob/main/MLP/datas/data_mv_sv_dv_ut_lt_hd_test.csv"
+response = requests.get(url)
+
+with open("filename.csv", "wb") as f:
+    f.write(response.content)
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
