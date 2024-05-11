c5212559350fad9d69df9c83bcb1aa466e645906
shy <shynare@gmail.com>
2024-05-12 03:58:00 +0900

    5월 11일

--- MLP.py
+++ MLP.py
@@ -5,18 +5,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import requests
+from io import StringIO
 
-url = "https://github.com/jinsobak/CMM_DeepLearning_Module/blob/main/MLP/datas/data_mv_sv_dv_ut_lt_hd_test.csv"
+url = "https://raw.githubusercontent.com/jinsobak/CMM_DeepLearning_Module/main/MLP/datas/data_mv_sv_dv_ut_lt_hd_test.csv"
 response = requests.get(url)
 
-with open("filename.csv", "wb") as f:
-    f.write(response.content)
+data = pd.read_csv(StringIO(response.text))
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+X = data.drop('label', axis=1)
+y = data['label'] 
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+plt.show()
