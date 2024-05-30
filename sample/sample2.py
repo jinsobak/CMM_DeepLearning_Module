9ec99ec88b4b99b478ec3e17fc27009a9300d6be
lyc3427@gmail.com <freeman3427@gmail.com>
2024-05-30 18:19:11 +0900

    wandb

--- sample/sample2.py
+++ sample/sample2.py
@@ -1,46 +1,98 @@
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+import wandb
+from wandb.integration.keras import WandbMetricsLogger
 
-# 데이터 불러오기
-data = pd.read_csv(
-    'C:\\Users\\ddc4k\\OneDrive\\Desktop\\빅브라더\\sample\\data_dv_hd.csv', encoding='cp949')
+# 데이터 전처리 및 준비
 
-# 입력과 타겟 데이터 나누기
-X = data.drop(columns=['품질상태', '품명'])
-y = data['품질상태']
+# wandb 초기화
+wandb.init(project='CIFAR10 Classification Example(Train)')
+# 실행 이름 설정
+wandb.run.name = 'First wandb'
+wandb.run.save()
 
-# 데이터 표준화
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
 
-# 훈련 데이터와 테스트 데이터 나누기
-X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42)
+def prepare_data(csv_file):
+    # CSV 파일을 읽어들여 DataFrame으로 변환
+    all_data = pd.read_csv(csv_file, encoding='cp949')
 
-# 모델 구성
-model = Sequential([
-    Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    Dense(32, activation='relu'),
-    Dense(16, activation='relu'),
-    Dense(1, activation='sigmoid')
+    # 특징 선택 (불필요한 열 제거 등)
+    selected_features = all_data.drop(
+        columns=['품질상태', '품명'])  # 품질상태와 품명을 제외한 특징 선택
+
+    # 딥러닝의 입력 데이터와 정답 데이터 생성
+    X = selected_features.values  # 입력 데이터
+    y = all_data['품질상태'].values  # 출력 데이터
+
+    # 테스트 데이터와 트레이닝 데이터로 분할
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+
+    # 데이터 스케일링
+    scalar = StandardScaler()
+    X_train_scaled = scalar.fit_transform(X_train)
+    X_test_scaled = scalar.transform(X_test)
+
+    return X_train_scaled, X_test_scaled, y_train, y_test, scalar
+
+
+# CSV 파일 경로
+csv_file = "C:\\Users\\ddc4k\\Onedrive\\Desktop\\빅브라더\\sample\\data_dv_ld.csv"
+
+# 데이터 전처리
+X_train, X_test, y_train, y_test, scalar = prepare_data(csv_file)
+
+# 특징과 레이블을 TensorFlow Dataset으로 변환합니다.
+train_dataset = tf.data.Dataset.from_tensor_slices(
+    (X_train, y_train)).shuffle(len(X_train)).batch(32)  # 배치 크기 조정
+test_dataset = tf.data.Dataset.from_tensor_slices(
+    (X_test, y_test)).batch(32)  # 배치 크기 조정
+
+# Early Stopping 설정
+early_stopping = EarlyStopping(
+    min_delta=0.001,  # 최소한의 변화
+    patience=20,      # 몇 번 연속으로 개선이 없는지
+    restore_best_weights=True  # 최상의 가중치로 복원
+)
+
+# 딥러닝 모델 구성
+model = models.Sequential([
+    layers.Dense(64, activation='relu',
+                 input_shape=(X_train.shape[1],)),  # 입력층
+    layers.Dense(32, activation='relu'),  # 은닉층
+    layers.Dense(16, activation='relu'),  # 은닉층 추가
+    layers.Dense(1, activation='sigmoid')  # 출력층 (이진 분류)
 ])
 
 # 모델 컴파일
-model.compile(optimizer=Adam(learning_rate=0.001),
-              loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=['accuracy', 'Precision', 'Recall'])
 
-# 모델 요약 확인
-model.summary()
-
-# 모델 훈련
-history = model.fit(X_train, y_train, batch_size=32,
-                    epochs=10, validation_split=0.2)
+# 모델 학습
+epochs = 100
+batch_size = 32
+history = model.fit(train_dataset, epochs=epochs, callbacks=[
+    early_stopping, WandbMetricsLogger()], validation_data=test_dataset)
 
 # 모델 평가
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print("Test Loss:", test_loss)
-print("Test Accuracy:", test_acc)
+loss, accuracy, precision, recall = model.evaluate(test_dataset)
+print(
+    f'Test Loss: {loss}, Test Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}')
+
+# wandb 설정 업데이트 및 로그 기록
+wandb.config.update({
+    "epochs": epochs,
+    "batch_size": batch_size
+})
+
+wandb.log({
+    "Test Loss": loss,
+    "Test Accuracy": accuracy,
+    "Test Precision": precision,
+    "Test Recall": recall
+})
