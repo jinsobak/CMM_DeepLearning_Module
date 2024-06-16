c029a84d63379644fb48a758c2d20b35f3c11c57
jinsobak <jinsobak@gmail.com>
2024-06-16 23:11:06 +0900

    june_16

--- MLP/EDA/linegraph_jd.py
+++ MLP/EDA/linegraph_jd.py
@@ -20,16 +20,17 @@ if __name__=="__main__":
     dataFrame2 = dataFrame.loc[dataFrame['품질상태'] == quality, :]
     print(dataFrame2.shape)
 
-    dataFrame2 = dataFrame2.loc[~(
-        (dataFrame2['판정_평면3 <- 점21, 점22, 점23, ... , 점26의 측정점 병합 <마이크로미터로 측정 해볼 것 !!!!!!!!!!!!!!!!>_Z'] > 0.645)
-        & (abs(dataFrame2['판정_점2 <- 점1의 되부름 <열전 관리치수(Spec : 116.6±0.1)>_X']) > 0.007) 
-        & (abs(dataFrame2['판정_직선21 <우하 소재>_X/Y']) > 0.07)
-    )]
 
-    dataFrame2 = dataFrame2.loc[~(
-        (abs(dataFrame2['판정_점2 <- 점1의 되부름 <열전 관리치수(Spec : 116.6±0.1)>_X']) > 0.04)
-         & (dataFrame2['판정_평면3 <- 점21, 점22, 점23, ... , 점26의 측정점 병합 <마이크로미터로 측정 해볼 것 !!!!!!!!!!!!!!!!>_Z'] > 0.645)
-    )]
+    # dataFrame2 = dataFrame2.loc[~(
+    #     (dataFrame2['판정_평면3 <- 점21, 점22, 점23, ... , 점26의 측정점 병합 <마이크로미터로 측정 해볼 것 !!!!!!!!!!!!!!!!>_Z'] > 0.645)
+    #     & (abs(dataFrame2['판정_점2 <- 점1의 되부름 <열전 관리치수(Spec : 116.6±0.1)>_X']) > 0.007) 
+    #     & (abs(dataFrame2['판정_직선21 <우하 소재>_X/Y']) > 0.07)
+    # )]
+
+    # dataFrame2 = dataFrame2.loc[~(
+    #     (abs(dataFrame2['판정_점2 <- 점1의 되부름 <열전 관리치수(Spec : 116.6±0.1)>_X']) > 0.04)
+    #      & (dataFrame2['판정_평면3 <- 점21, 점22, 점23, ... , 점26의 측정점 병합 <마이크로미터로 측정 해볼 것 !!!!!!!!!!!!!!!!>_Z'] > 0.645)
+    # )]
 
     # dataFrame2 = dataFrame2.loc[(
     #     (abs(dataFrame2['판정_점13 <- 점11와 점12의 중점 <열전관리_상>_X']) == 0) 
