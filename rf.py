30853ac1c9f65364f00e0f3d5ba65d42179b13db
이승훈 <162983589+shynare@users.noreply.github.com>
2024-07-12 15:48:55 +0900

    Update rf.py

--- rf.py
+++ rf.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
 # 데이터 파일을 제대로 불러오기 위해 파일 경로와 인코딩을 확인합니다.
 df = pd.read_csv("C:/Users/win/Desktop/data_mv_sv_dv_ut_lt_hd_no_NTC.csv", encoding='cp949')
@@ -25,14 +26,30 @@ feature_columns = list(df.columns.difference(['품질상태']))
 X = df[feature_columns]
 y = df['품질상태']
 
+# 스케일링
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 # 학습 데이터와 평가 데이터로 분할합니다.
 train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=42)
 print(train_x.shape, test_x.shape, train_y.shape, test_y.shape)  # 데이터 개수 확인
 
-# 랜덤 포레스트 분류기를 생성하고 학습합니다.
-clf = RandomForestClassifier(n_estimators=100, max_depth=100, random_state=0)
-clf.fit(train_x, train_y)
+# 랜덤 포레스트 분류기 하이퍼파라미터 튜닝
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'bootstrap': [True, False]
+}
+
+clf = RandomForestClassifier(random_state=42)
+grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+grid_search.fit(train_x, train_y)
+
+best_clf = grid_search.best_estimator_
 
 # 평가 데이터로 예측을 수행하고 정확도를 출력합니다.
-predict2 = clf.predict(test_x)
-print(accuracy_score(test_y, predict2))
+predict2 = best_clf.predict(test_x)
+print("Best parameters found: ", grid_search.best_params_)
+print("Accuracy: ", accuracy_score(test_y, predict2))
